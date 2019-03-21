9bd3a547955e457f23bad8d685605138b0d3638a
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-21 20:37:39 +0200

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -14,7 +14,6 @@ async def on_ready():
     
 bot.remove_command('help')
 @Mike.command(pass_context = True)
-@commands.has_permissions(manage_messages = True)
 async def say(ctx, *args):
     mesg = ' '.join(args)
     await Mike.delete_message(ctx.message)
