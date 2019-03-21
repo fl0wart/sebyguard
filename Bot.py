8122e9bc7e1d0acec9a8c552fb864b9a628537ee
fl0wart <47152380+fl0wart@users.noreply.github.com>
2019-03-21 10:31:31 +0200

    Create Bot.py

---
+++ Bot.py
@@ -0,0 +1,22 @@
+import asyncio
+import discord
+from discord.ext.commands import Bot
+
+
+Mike = Bot('p!')
+
+@Mike.event
+async def on_ready():
+    print ("Starting up")
+    print ("My username is " + Mike.user.name + " and i am running with the ID: " + Mike.user.id)
+    await Mike.change_presence(game=discord.Game(name="the infractions! 👀", type=3))
+    print ("Started")
+    
+@Mike.command(pass_context = True)
+async def say(ctx, *args):
+    mesg = ' '.join(args)
+    await Mike.delete_message(ctx.message)
+    return await Mike.say(mesg)
+
+
+Mike.run('NTU4MjA1MDQxOTA3OTkwNTI4.D3Tcig.erxYozqaEr6gZBqZfex-EWJ-7ik')
